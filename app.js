652d24587261734163d55bb827542fed83c224e2
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-06-08 17:42:40 -0500

    added for loop that creates desired number of divs for game board

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
+//for loop that creates 200 divs for tetris board
+
+    for (var i = 0; i < 200; i++) {
+        let block = document.createElement('div');
+        board.appendChild(block); 
+    
+    };
 
   
 
