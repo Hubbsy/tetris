3ae1e28ce00dcdf8e7d0292b5268ea5fb330d5ad
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-06-08 18:12:34 -0500

    added variables for grid squares and score keeping

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
     
     };
 
-  
+//tetrominos
 
+    const grid = document.querySelector('.grid'); 
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
+    const width = 10;
+
+ 
 
 })
