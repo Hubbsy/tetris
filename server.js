3899ead2d8010bb988c67496ec8a8de92d70ec43
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-06-24 21:02:58 -0500

    set up code to serve index.html static file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 
 const app = express();
 
 const PORT = process.env.PORT || 3000
 
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/style.css'));
+// });
 
 
 app.listen(PORT, () => console.log(`Houston we have a connection at port ${PORT}`));
