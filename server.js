e85cf1efdb03c963ff0af24fae76971e5acc6da3
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-06-24 21:11:56 -0500

    moved static files to public directory to serve with express

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/style.css'));
